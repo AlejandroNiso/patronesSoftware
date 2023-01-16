e55db109703fe2a0c034162921d4630984f5ad51
MariaRodrigo <maria.rogon99@gmail.com>
2023-01-16 13:00:42 +0100

    .

---
+++ Almacen/src/main/java/com/mycompany/almacen/Almacen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.almacen;
+
+/**
+ *
+ * @author maria
+ */
+public class Almacen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
